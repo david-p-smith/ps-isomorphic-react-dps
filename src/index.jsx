128e6d4482dab978c1aff0116c7e59a00e3f9742
DPS <smith.dp@gmail.com>
2020-04-21 10:12:17 +0100

    add redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import getStore from './getStore';
+import { Provider } from 'react-redux';
+
+const store = getStore();
 
 const render = (App) => {
   ReactDOM.render(
-    <App />,
+    <Provider store={store} >
+      <App />
+    </Provider>,
     document.getElementById('AppContainer')
   )
 };
