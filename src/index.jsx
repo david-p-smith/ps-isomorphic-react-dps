3272e0420e7ac7805d592fba60cc7f1a5385819f
DPS <smith.dp@gmail.com>
2020-04-21 09:18:21 +0100

    add basic App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.info("React is cool!");
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const render = (App) => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('AppContainer')
+  )
+};
+
+render(App);
